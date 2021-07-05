61d45ebe4a6810e499c0236bf7c7ab3aed9d9198
SitiNabila30 <86936881+SitiNabila30@users.noreply.github.com>
2021-07-05 11:58:37 +0700

    Update evaluasi aritmatika.cpp

--- evaluasi aritmatika.cpp
+++ evaluasi aritmatika.cpp
@@ -1,68 +1,8 @@
-#include<bits/stdc++.h>
-using namespace std;
+#include <iostream>
 
+Using namespace std;
 
-int prec(char c) {
-    if(c == '*' || c == '/' || c == '%') 
-  return 2; 
-  else if(c == '+' || c == '-') 
-  return 1; 
-    else
- return -2;
-}
- 
+int main () {
 
-void infixToPostfix(string s) {
- 
-    stack<char> st; 
-    string result;
- 
-    for(int i = 0; i < s.length(); i++) {
-        char c = s[i];
- 
-        
-        if(c >= '0' && c <= '9')
-            result += c;
- 
-        
-        else if(c == '(')
-            st.push('(');
- 
-        
-        else if(c == ')') {
-            while(st.top() != '(')
-            {
-                result += st.top();
-                st.pop();
-            }
-            st.pop();
-        }
- 
-        
-        else {
-            while(!st.empty() && prec(s[i]) <= prec(st.top())) {
-                result += st.top();
-                st.pop(); 
-            }
-            st.push(c);
-        }
-    }
- 
-    
-    while(!st.empty()) {
-        result += st.top();
-        st.pop();
-    }
- 
-    cout << result << "  "<< endl;
-}
- 
-
-int main() {
-    string a ;
-    cin >> a;    
-    infixToPostfix(a);
-   // cout << result << "  "<< endl;
-    
-    return 0;
+return 0;
 }

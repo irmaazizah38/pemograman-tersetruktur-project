c2a802945018e04b11a01295597b12f14db5e2cc
irmaazizah38 <86936133+irmaazizah38@users.noreply.github.com>
2021-07-05 11:43:52 +0700

    Fungsi dengan operand bilangan positif

--- ekspresi matematika.cpp
+++ ekspresi matematika.cpp
@@ -1,8 +1,70 @@
-#include <iostream>
+// CONVERT INFIX TO POSTFIX
 
+#include<bits/stdc++.h>
 using namespace std;
 
-int main (){
-  
-  return 0;
+
+int prec(char c) {
+    if(c == '*' || c == '/' || c == '%') 
+  return 2; 
+  else if(c == '+' || c == '-') 
+  return 1; 
+    else
+ return -2;
 }
+ 
+
+void infixToPostfix(string s) {
+ 
+    stack<char> st; 
+    string result;
+ 
+    for(int i = 0; i < s.length(); i++) {
+        char c = s[i];
+ 
+        
+        if(c >= '0' && c <= '9')
+            result += c;
+ 
+        
+        else if(c == '(')
+            st.push('(');
+ 
+        
+        else if(c == ')') {
+            while(st.top() != '(')
+            {
+                result += st.top();
+                st.pop();
+            }
+            st.pop();
+        }
+ 
+        
+        else {
+            while(!st.empty() && prec(s[i]) <= prec(st.top())) {
+                result += st.top();
+                st.pop(); 
+            }
+            st.push(c);
+        }
+    }
+ 
+    
+    while(!st.empty()) {
+        result += st.top();
+        st.pop();
+    }
+ 
+    cout << result << "  "<< endl;
+}
+ 
+
+int main() {
+    string a ;
+    cin >> a;    
+    infixToPostfix(a);
+   // cout << result << "  "<< endl;
+    
+    return 0;
+}   

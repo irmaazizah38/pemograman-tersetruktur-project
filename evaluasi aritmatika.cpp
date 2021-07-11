c7daf742c3cb02f077655ebed4f7ce257a742fc8
irmaazizah38 <86936133+irmaazizah38@users.noreply.github.com>
2021-07-12 00:36:20 +0700

    fungsi hasil

--- evaluasi aritmatika.cpp
+++ evaluasi aritmatika.cpp
@@ -70,3 +70,139 @@ for(i=0; i<in.length(); i++){
 void hasil(string in){
     stack<double> data;
     stack<char>operasi;
+for(i=0; i<in.length(); i++){
+        if(isdigit(in[i])){
+            int nilai=0;
+            while(i<in.length() && isdigit(in[i])){
+                nilai = (nilai*10) + (in[i] - '0');
+                i++;
+            }
+            i--;
+            data.push(nilai);
+        }
+            else if(in[i] == '(')
+                operasi.push(in[i]);
+            else if(in[i] == ')'){
+                while(!operasi.empty() && operasi.top() != '('){
+                    if(operasi.top() == '%'){
+                        int kedua = data.top(); data.pop();
+                        int pertama  = data.top(); data.pop();
+                        operasi.pop();
+                        data.push(pertama %kedua);
+                        
+                    } 
+                    else
+                         {
+                        double kedua = data.top(); data.pop();
+                        double pertama  = data.top(); data.pop();
+                        char opr = operasi.top(); operasi.pop();
+                        data.push(applyOpr(pertama , kedua, opr));
+                    }
+                }
+                
+                if(!operasi.empty())
+                    operasi.pop();
+                    
+            } 
+            else 
+                {
+                if(in[i] == '-'){
+                    if(i==0){
+                        if(isdigit(in[i+1])){
+                            i++;
+                            int nilai=0;
+                            while(i<in.length() && isdigit(in[i])){
+                                nilai = (nilai*10) + (in[i] - '0');
+                                i++;
+                            }
+                            i--;
+                            data.push(nilai*-1);
+                        } else {
+                            data.push(-1);
+                            operasi.push('*');
+                        }
+                        
+                    } 
+                    else 
+                        {
+                        while(!operasi.empty() && parse(operasi.top()) >= parse(in[i]) && !Symbol(in[i-1])){
+                            if(operasi.top() == '%'){
+                                int kedua = data.top(); data.pop();
+                                int pertama  = data.top(); data.pop();
+                                operasi.pop();
+                                data.push(pertama %kedua);
+                            
+                            } 
+                            else 
+                                {
+                                double kedua = data.top(); data.pop();
+                                double pertama  = data.top(); data.pop();
+                                char opr = operasi.top(); operasi.pop();
+                                data.push(applyOpr(pertama , kedua, opr));
+                            }
+                        }
+                        if((isdigit(in[i+1]) || in[i+1]=='(') && (isdigit(in[i-1]) || in[i-1]==')'))
+                            operasi.push(in[i]);
+                        
+                        else
+                            {
+                            data.push(-1);
+                            operasi.push('*');
+                        }
+                    } 
+                    
+                } 
+                else 
+                    {
+                    while(!operasi.empty() && parse(operasi.top()) >= parse(in[i])){
+                        if(operasi.top() == '%'){
+                            int kedua = data.top(); data.pop();
+                            int pertama  = data.top(); data.pop();
+                            operasi.pop();
+                            data.push(pertama %kedua);
+                        } 
+                        else 
+                            {
+                            double kedua = data.top(); data.pop();
+                            double pertama  = data.top(); data.pop();
+                            char opr = operasi.top(); operasi.pop();
+                            data.push(applyOpr(pertama , kedua, opr));
+                        }
+                    }
+                    operasi.push(in[i]);
+                }
+            }
+        }
+        
+        while(!operasi.empty()){
+            if(operasi.top() == '%'){
+                int kedua = data.top(); data.pop();
+                int pertama  = data.top(); data.pop();
+                operasi.pop();
+                data.push(pertama %kedua);
+         
+            } 
+            else 
+                {
+                double kedua = data.top(); data.pop();
+                double pertama  = data.top(); data.pop();
+                char opr = operasi.top(); operasi.pop();
+                data.push(applyOpr(pertama , kedua, opr));
+            }
+        }
+        cout << data.top() << endl;
+    }
+
+int main(){
+    string ekspresi; getline(cin,ekspresi);
+    string baru_ekspresi="";
+    for(i=0;i<ekspresi.length();i++){
+        if(ekspresi[i]==' ')
+            continue;
+        else
+            baru_ekspresi+=ekspresi[i];
+    }
+    hasil(baru_ekspresi);
+
+    return 0;
+}
